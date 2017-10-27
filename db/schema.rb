25b00e21c0af80f856d6058572fab09d99f650cc
kato11433 <s1511433@u.tsukuba.ac.jp>
2017-10-27 08:32:53 +0000

    共有

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018040809) do
+ActiveRecord::Schema.define(version: 20171027070705) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "item_id"
+    t.string "comment_body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "departments", force: :cascade do |t|
     t.string "name"
