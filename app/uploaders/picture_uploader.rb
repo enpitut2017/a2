8b80682bac581d402007ac7cb263a6ba28ad9569
Kousuke <sasaki-trombone@users.noreply.github.com>
2017-12-06 12:41:37 +0900

    ローカルではローカルに画像を保存、herokuではIDCFに画像を保存する

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -12,7 +12,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   #   storage :file
   # end
 
-  storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
