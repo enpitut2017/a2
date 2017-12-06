c7ec875b4837a1da32bd4e296e164178351d710a
Kousuke <sasaki-trombone@users.noreply.github.com>
2017-12-06 12:50:13 +0900

    環境変数書き換え

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
     scheme:                'https',
     aws_access_key_id:     ENV['IDCF_ACCESS_KEY'], # アクセスキーを記述
     aws_secret_access_key: ENV['IDCF_SECRET_KEY'], # シークレットキー
-    region:                'ap-northeast-1', # 東京
+    region:                ENV['IDCF_REGION'], # 東京
     aws_signature_version: 2
   }
   config.fog_directory = ENV['IDCF_BUCKET'] # バケット名を記述
