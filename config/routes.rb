c770993c4b2355f6f68efcc5994bbc97e5651d89
橋爪友莉子 <yuyu1.run@icloud.com>
2018-01-11 18:00:01 +0900

    メールを送るのをコントローラーにやらせる

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,12 @@ Rails.application.routes.draw do
 
 post "items/comment" => "items#comment"
 post "items/destroy" => "items#destroy"
+post "items/done" => "items#done"
 
   get 'mail_page/home'
   get 'mail_page/done'
+  get 'mail_page/done2'
+  get 'mail_page/cancel'
 
   post "items/:id" => "items#show"
 
