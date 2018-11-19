103f09227250eff91509d5e05dbac6922363c1cf
omiya-pan <omiya-pan@users.noreply.github.com>
2018-11-19 03:34:03 +0000

    render

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module A2
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
+    config.web_console.whitelisted_ips = '10.0.2.2'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
